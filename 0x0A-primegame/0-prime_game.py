2e33ddcb2aa6621a1e3b15e7b6f76db4a9c770e3
amr12121 <eltohmyamr@gmail.com>
2024-12-12 23:49:56 +0000

    a

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,16 +1,39 @@
-primeCheck = int(input("Enter a number: "))
+def isWinner(x, nums):
+    if x < 1 or not nums:
+        return None
 
-if primeCheck <= 1:
-    print("Not a prime number")  
-else:
-    is_prime = True  
-    for i in range(2, int(primeCheck ** 0.5) + 1):
-           if primeCheck % i == 0:
-            is_prime = False  
-            break
+    # Find the maximum number in nums
+    max_n = max(nums)
 
-    if is_prime:
-        print("Prime")
+    # Sieve of Eratosthenes to determine prime numbers up to max_n
+    primes = [True] * (max_n + 1)
+    primes[0] = primes[1] = False  # 0 and 1 are not prime
+    for i in range(2, int(max_n ** 0.5) + 1):
+        if primes[i]:
+            for j in range(i * i, max_n + 1, i):
+                primes[j] = False
+
+    # Precompute the number of primes up to each number
+    prime_count = [0] * (max_n + 1)
+    for i in range(1, max_n + 1):
+        prime_count[i] = prime_count[i - 1] + (1 if primes[i] else 0)
+
+    # Determine the winner for each round
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        # If the number of primes up to n is odd, Maria wins; otherwise, Ben wins
+        if prime_count[n] % 2 == 1:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    # Determine the overall winner
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
     else:
-        print("Not prime")
+        return None
 
